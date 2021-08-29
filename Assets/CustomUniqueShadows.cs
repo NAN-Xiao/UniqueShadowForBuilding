e8e009dc3636cbc63f89a173e4c550be2a52ddf4
肖楠 <7367501@qq.com>
2021-08-29 21:34:30 +0800

    更新阴影

--- Assets/CustomUniqueShadows.cs
+++ Assets/CustomUniqueShadows.cs
@@ -13,27 +13,55 @@ public class CustomUniqueShadows : MonoBehaviour
 
     Renderer[] m_renders;
 
+    float m_far;
 
     float m_radius;
     //public
 
     public Light m_shadowLight;
 
-    public int m_shadowMapSize=2048;
+    public int m_shadowMapSize = 2048;
     public RenderTexture m_shadowMap;
 
     public float orthographicSize;
 
+    List<Material> m_mts;
+
+    Matrix4x4 m_shadowMatrix;
+    float m_shadowFilterWidth=10;
 
 
+    private float m_bias;
+
+    const string UNIQUE_SHADOW = "UNIQUE_SHADOW";
+    const string _UniqueShadowMatrix = "_UniqueShadowMatrix";
+    const string _UniqueShadowFilterWidth = "_UniqueShadowFilterWidth";
+    const string _UniqueShadowTexture = "_UniqueShadowTexture";
     // Start is called before the first frame update
     void Start()
     {
         //test
         InitCamera();
         GetRenderTarget();
+
         m_renders = transform.GetComponentsInChildren<Renderer>();
+        m_mts = new List<Material>();
+        foreach (var r in m_renders)
+        {
+            var mt = r.sharedMaterials;
+            foreach (var m in mt)
+            {
+                if (m_mts.Contains(m)) continue;
+                m_mts.Add(m);
+            }
+
+
+        }
+
+        m_shadowFilterWidth /= m_shadowMapSize;
     }
+
+
 
     // Update is called once per frame
     void Update()
@@ -45,20 +73,44 @@ public class CustomUniqueShadows : MonoBehaviour
     void UpdateCamera()
     {
         //var lightdir = m_shadowLight.transform.position- transform.position;
-        m_shadowCamera.transform.position = m_bounds.center-m_shadowLight.transform.forward* m_radius;
+        m_shadowCamera.transform.position = m_bounds.center - m_shadowLight.transform.forward * m_radius;
         m_shadowCamera.transform.rotation = m_shadowLight.transform.rotation;
         m_shadowCamera.orthographicSize = m_radius;
-        m_shadowCamera.farClipPlane = Vector3.Distance(m_shadowCamera.transform.position, m_bounds.center) + m_radius;   
-    }
-     void SetUniforms()
-    {
-        for (int i = 0, n = m_materialInstances.Count; i < n; ++i)
+        m_shadowCamera.farClipPlane = Vector3.Distance(m_shadowCamera.transform.position, m_bounds.center) + m_radius;
+
+        var shadowProjection = Matrix4x4.Ortho(-m_radius, m_radius, -m_radius, m_radius, m_shadowCamera.nearClipPlane, m_shadowCamera.farClipPlane);
+
+        m_bias = m_shadowLight.shadowBias;
+        var db = SystemInfo.usesReversedZBuffer ? m_bias : -m_bias;
+        var m_shadowSpaceMatrix = Matrix4x4.identity;
+        m_shadowSpaceMatrix.SetRow(0, new Vector4(0.5f, 0.0f, 0.0f, 0.5f));
+        m_shadowSpaceMatrix.SetRow(1, new Vector4(0.0f, 0.5f, 0.0f, 0.5f));
+        m_shadowSpaceMatrix.SetRow(2, new Vector4(0.0f, 0.0f, 0.5f, 0.5f + db));
+        m_shadowSpaceMatrix.SetRow(3, new Vector4(0.0f, 0.0f, 0.0f, 1.0f));
+
+
+       
+        var camproj = m_shadowCamera.projectionMatrix;
+        if (SystemInfo.usesReversedZBuffer)
         {
-            var m = m_materialInstances[i];
+            camproj[2, 0] = -shadowProjection[2, 0];
+            camproj[2, 1] = -shadowProjection[2, 1];
+            camproj[2, 2] = -shadowProjection[2, 2];
+            camproj[2, 3] = -shadowProjection[2, 3];
+        }
+        m_shadowMatrix = m_shadowSpaceMatrix * camproj * m_shadowCamera.worldToCameraMatrix;
+
+        SetUniforms();
+    }
+    void SetUniforms()
+    {
+        for (int i = 0, n = m_mts.Count; i < n; ++i)
+        {
+            var m = m_mts[i];
             m.EnableKeyword(UNIQUE_SHADOW);
-            m.SetMatrix(Uniforms._UniqueShadowMatrix, m_shadowMatrix);
-            m.SetFloat(Uniforms._UniqueShadowFilterWidth, m_shadowFilterWidth);
-            m.SetTexture(Uniforms._UniqueShadowTexture, m_UniqueShadowTexture);
+            m.SetMatrix(_UniqueShadowMatrix, m_shadowMatrix);
+            m.SetFloat(_UniqueShadowFilterWidth, m_shadowFilterWidth);
+            m.SetTexture(_UniqueShadowTexture, m_shadowMap);
         }
     }
 
@@ -69,24 +121,24 @@ public class CustomUniqueShadows : MonoBehaviour
             return;
         }
         m_bounds = new Bounds(transform.position, Vector3.one * 0.1f);
-    
+
         foreach (var m in m_renders)
         {
             m_bounds.Encapsulate(m.bounds);
         }
         m_radius = m_bounds.extents.magnitude;
-        Debug.LogError(m_bounds.extents.magnitude);
+        //Debug.LogError(m_bounds.extents.magnitude);
 
 
     }
 
     void InitCamera()
     {
-       if(m_shadowCamera==null)
+        if (m_shadowCamera == null)
         {
             m_shadowCamera = new GameObject("_UniqueShadow").AddComponent<Camera>();
         }
-      //  m_shadowCamera.renderingPath = RenderingPath.Forward;
+        //  m_shadowCamera.renderingPath = RenderingPath.Forward;
         m_shadowCamera.clearFlags = CameraClearFlags.Depth;
         m_shadowCamera.depthTextureMode = DepthTextureMode.Depth;
         m_shadowCamera.useOcclusionCulling = false;
@@ -94,7 +146,7 @@ public class CustomUniqueShadows : MonoBehaviour
         m_shadowCamera.orthographic = true;
         m_shadowCamera.depth = -100;
         m_shadowCamera.aspect = 1f;
-      //  m_shadowCamera.enabled = false;
+        //  m_shadowCamera.enabled = false;
 
     }
 
@@ -107,7 +159,7 @@ public class CustomUniqueShadows : MonoBehaviour
              : RenderTextureFormat.Depth;
 
             m_shadowMap = RenderTexture.GetTemporary(m_shadowMapSize, m_shadowMapSize, 16, fm);
-            m_shadowMap.filterMode = FilterMode.Bilinear;
+            m_shadowMap.filterMode = FilterMode.Point;
             m_shadowMap.wrapMode = TextureWrapMode.Clamp;
 
         }
@@ -119,7 +171,7 @@ public class CustomUniqueShadows : MonoBehaviour
         //remove camera;
         //clear mesh;
         //clear bounds;
-        
+
     }
 
 
@@ -127,7 +179,7 @@ public class CustomUniqueShadows : MonoBehaviour
     {
 
 
-        if(m_bounds!=null)
+        if (m_bounds != null)
         {
 
             Gizmos.DrawWireSphere(m_bounds.center, m_bounds.extents.magnitude);
--- Assets/NewUnlitShader.shader
+++ Assets/NewUnlitShader.shader
@@ -3,6 +3,8 @@
     Properties
     {
         _MainTex ("Texture", 2D) = "white" {}
+
+        _UniqueShadowTexture ("Texture", 2D) = "white" {}
     }
     SubShader
     {
@@ -18,10 +20,43 @@
             #pragma multi_compile_fog
 
             #include "UnityCG.cginc"
-
-
+            #include "UnityShadowLibrary.cginc"
             sampler2D _UniqueShadowTexture;
-            float4x4 
+            //TEXTURE2D_SHADOW(_UniqueShadowTexture);//
+            //SAMPLER_CMP(sampler_UniqueShadowTexture);
+            float _UniqueShadowFilterWidth;
+            float4x4 _UniqueShadowMatrix;
+
+
+            static half2 poisson[8] =
+            {
+                half2(0.02971195f, -0.8905211f),
+                half2(0.2495298f, 0.732075f),
+                half2(-0.3469206f, -0.6437836f),
+                half2(-0.01878909f, 0.4827394f),
+                half2(-0.2725213f, -0.896188f),
+                half2(-0.6814336f, 0.6480481f),
+                half2(0.4152045f, -0.2794172f),
+                half2(0.1310554f, 0.2675925f),
+            };
+
+            half SampleUnique(half4 shadowCoord)
+            {
+                half4 uv = shadowCoord;
+                half shadow = 0.f;
+                for(int i = 0; i < 8; ++i)
+                {
+                    uv.xy = shadowCoord.xy + poisson[i]*_UniqueShadowFilterWidth;
+                    shadow += (shadowCoord.z  > DecodeFloatRGBA(tex2D(_UniqueShadowTexture,uv)) ? 1.0 : 0.0);   
+                }        
+                return shadow /8.0f;
+            }
+
+            float4 TransformWorldToShadowCoord(float3 positionWS)
+            {
+                return mul(_UniqueShadowMatrix, float4(positionWS, 1.0));
+            }
+
             struct appdata
             {
                 float4 vertex : POSITION;
@@ -33,6 +68,7 @@
                 float2 uv : TEXCOORD0;
                 UNITY_FOG_COORDS(1)
                 float4 vertex : SV_POSITION;
+                float4 shadowcoord:texcoord2;
             };
 
             sampler2D _MainTex;
@@ -43,48 +79,22 @@
                 v2f o;
                 o.vertex = UnityObjectToClipPos(v.vertex);
                 o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                o.shadowcoord=TransformWorldToShadowCoord(mul(unity_ObjectToWorld,v.vertex));
                 UNITY_TRANSFER_FOG(o,o.vertex);
                 return o;
             }
 
             fixed4 frag (v2f i) : SV_Target
             {
-                // sample the texture
+                // sample the texture//
                 fixed4 col = tex2D(_MainTex, i.uv);
+                col.rgb*=SampleUnique(i.shadowcoord);
+              //  col.rgb=LinearEyeDepth(SAMPLE_DEPTH_TEXTURE(_UniqueShadowTexture,i.shadowcoord));
                 // apply fog
                 UNITY_APPLY_FOG(i.fogCoord, col);
                 return col;
             }
 
-static half2 poisson[8] =
- {
-	half2(0.02971195f, -0.8905211f),
-	half2(0.2495298f, 0.732075f),
-	half2(-0.3469206f, -0.6437836f),
-	half2(-0.01878909f, 0.4827394f),
-	half2(-0.2725213f, -0.896188f),
-	half2(-0.6814336f, 0.6480481f),
-	half2(0.4152045f, -0.2794172f),
-	half2(0.1310554f, 0.2675925f),
-};
-
-half SampleUnique(half4 shadowCoord)
-{
-    half4 uv = shadowCoord;
-	half shadow = 0.f;
-	for(int i = 0; i < 8; ++i) {
-		uv.xy = shadowCoord.xy + poisson[i] * _UniqueShadowFilterWidth;
-        shadow += SAMPLE_TEXTURE2D_SHADOW(_UniqueShadowTexture, sampler_UniqueShadowTexture, uv.xyz).x;
-	} 
-	return shadow / 8.f;
-   // return SAMPLE_TEXTURE2D_SHADOW(_UniqueShadowTexture, sampler_UniqueShadowTexture, shadowCoord.xyz).x;
-
-}
-
-float4 TransformWorldToShadowCoord(float3 positionWS)
-{
- return mul(_UniqueShadowMatrix, float4(positionWS, 1.0));
-}
 
 
             ENDCG
